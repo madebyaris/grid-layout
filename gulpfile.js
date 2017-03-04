c4bd3f20442159ee0be24989a52cdfce8f4e0749
aris <arissetia.m@gmail.com>
2017-03-04 22:48:06 +0700

    commit for inital 2nd

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    gulpSass = require('gulp-sass'),
+    gulpConcat = require('gulp-concat'),
+    browserSync = require('browser-sync');
+
+var config = {
+    bowerDir: './bower_components'
+}
+
+
+
+//convert sass to css
+
+//make good thing for js
+
+//compile all
+
+//watch
+
+//default
