98d68a26cc76be7395e23e38691a7ed032f1d8cf
aris <arissetia.m@gmail.com>
2017-03-06 08:25:09 +0700

    thinking, if we need to create prod and dev stage

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,26 @@ var config = {
 
 
 
-//convert sass to css
+//convert sass to css dev
+gulp.task('styles:dev', function() {
+    return gulp.src([
+            'assets/css/sass/*.sass'
+        ])
+        .pipe(gulpSass().on('error', gulpSass.logError))
+        .pipe(gulp.dest('assets/css/'))
+        .pipe(browserSync.stream());
+});
+
+//convert sass to css production
+gulp.task('styles:production', function() {
+    return gulp.src([
+            'assets/css/sass/*.sass'
+        ])
+        .pipe(gulpSass({ outputStyle: 'compressed' }).on('error', gulpSass.logError))
+        .pipe(gulp.dest('assets/css/'))
+        .pipe(browserSync.stream());
+});
+
 
 //make good thing for js
 
